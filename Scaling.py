c71e58bdb8fb1195c59f2f209454458c50b2536b
Jason Tran <jtran1@stevens.edu>
2019-08-02 00:40:25 -0400

    Cleaning up the program and adding comments

--- Scaling.py
+++ Scaling.py
@@ -5,6 +5,7 @@ from PIL import Image
 import glob
 
 outputFile = open("Scaling_Results.txt", "w")
+# simple function to calculate the average of a list
 def getListAverage(list):
     numElements = 0
     sum = 0
@@ -15,41 +16,36 @@ def getListAverage(list):
     return sum / numElements
 
 def getScaling(heatmap, groundTruth):
-    scale_avg = []
+    # scale_avg = []
     scale = []
     numElements = 0
 
+    # Cropping both images
     width, height = heatmap.size
-    area = (width * .48, height * .93, width * .51, height * .95)
+    area = (width * .48, height * .95, width * .51, height * .97)
     cropped_img = heatmap.crop(area)
     cropped_Truth = groundTruth.crop(area)
     pix_val = list(cropped_img.getdata())
     ground_pix = list(cropped_Truth.getdata())
 
-    for i in range(len(ground_pix)):
-        ground_pix[i] = ground_pix[i] / 256
-    scale_avg.append(getListAverage(pix_val) / getListAverage(ground_pix))
-
+    # Compare pixel by pixel and store to later calculate the average. Divide ground_pix by 256 to get an 8 bit value
     for i in range(len(ground_pix)):
         if(ground_pix[i] != 0):
+            ground_pix[i] = ground_pix[i] / 256
             scale.append(pix_val[i] / ground_pix[i])
             numElements += 1
-    scale_avg.append(scale)
-    return scale_avg
+    return scale
 
+# Simple function that simply outputs the data in a way that can be read in a text file
 def writeScaleToFile(scale, heatName, truthName):
-    outputFile.write("Average of heatmap -" +  heatName
-    + "- divide by average of ground truth -"
-    + truthName + "-: " + str(scale.pop(0)) + "\n")
-
-    outputFile.write("Average of every individual pixel of heatmap -" + heatName
-    + "- divided by ground truth-" + truthName + "-: "
-    + str(getListAverage(scale.pop())) + "\n")
+    outputFile.write("Heatmap: " + heatName
+    + "GroundTruth: " + truthName + " Scaling: "
+    + str(getListAverage(scale)) + "\n")
 
     outputFile.write("\n")
 
 
-
+# Basically the main of the program
 fileList = glob.glob('Images/*.png')
 groundTruths = glob.glob('disp_noc_0/*.png')
 
